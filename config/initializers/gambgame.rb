34518233b45293e144cd48a51aee0d2c9938118d
Carlos Fernandez <ferlocar.tec@gmail.com>
2014-06-27 14:16:26 -0600

    Make sure app doesnt fail when trying to check for games

--- config/initializers/gambgame.rb
+++ config/initializers/gambgame.rb
@@ -1,7 +1,12 @@
 Thread.new do
-  loop do
-    puts "Checking if the Gambgame has ended."
-    GambgameController.check_ongoing_gambgame
-    sleep 60
+  begin
+    loop do
+      puts "Checking if the Gambgame has ended."
+      GambgameController.check_ongoing_gambgame
+      sleep 60
+    end
+  rescue Exception => e
+    puts "Could not check Gambgame. A problem with database perhaps?"
+    puts e.message
   end
 end
